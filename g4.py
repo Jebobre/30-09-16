b7607a1bdc9ef7fd6011dfb2ae9dfdf8b42ae6d1
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-30 14:16:16 +0300

    h

--- g4.py
+++ g4.py
@@ -1,3 +1,7 @@
 __author__ = 'student'
-A[1,2,3,4,2]
+A=[1,2,3,4,2]
 t=2
+for i in range(0,t):
+    x=A.pop()
+    A.insert(-x,x)
+print(A)
---
+++ g6.py
@@ -0,0 +1,5 @@
+__author__ = 'student'
+N=7
+A=[6,1,9,2,3,4,8]
+for i in range(0,len(A)):
+    if A[i]
